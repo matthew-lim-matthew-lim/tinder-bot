accf2a0e1ef26eb2006e819628fa050c06cefe35
matthew-lim-matthew-lim <matthewlimcenterdiv@gmail.com>
2024-11-19 21:00:33 +1100

    add measures to prevent bot detection: logging in each swipe session, pauses before beginning to swipe

--- run_bot.py
+++ run_bot.py
@@ -6,8 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-tinder = Tinder(debug=True, x_auth_token=os.getenv('AUTH_TOKEN'))
-
 hrs_between_swipes = int(input("How many hours between swipes? "))
 
 while True:
@@ -16,24 +14,30 @@ while True:
         wait_time = hrs_between_swipes * 60 * 60 + random.randint(0, 3600)
         time.sleep(wait_time)
 
-    users_to_swipe = tinder.swipe.get_users()
+    # Make a new instance of Tinder every session of swiping, avoiding suspicion.
+    with Tinder(debug=True, x_auth_token=os.getenv('AUTH_TOKEN')) as tinder:
+        users_to_swipe = tinder.swipe.get_users()
 
-    for user in users_to_swipe:
-        # print(user)
-        if random.random() < 0.5:
-            # LIKE USER
-            liked = tinder.swipe.like_user(user["user_id"]) 
-            # print("#### Liked")
-            # print(liked) # -> {'status': 200, 'match': False, 'user_id': 'some_user_id', 'likes_left': 100}
-
-            # TODO: different behavior if match is True
-        else: 
-            # PASS USER
-            tinder.swipe.pass_user(user["user_id"]) 
-            # print("#### Not liked")
-        # Wait between 1 and 10 seconds because we aren't a bot ;)
+        # Wait a lil bit before swiping to avoid suspicion xD
         time.sleep(max(1, random.random() * 10))
 
-    # Swipe again tommorow!
-    wait_time = hrs_between_swipes * 60 * 60 + random.randint(0, 3600)
-    time.sleep(wait_time)
+        for user in users_to_swipe:
+            # print(user)
+            if random.random() < 0.5:
+                # LIKE USER
+                liked = tinder.swipe.like_user(user["user_id"]) 
+                # print("#### Liked")
+                # print(liked) # -> {'status': 200, 'match': False, 'user_id': 'some_user_id', 'likes_left': 100}
+
+                # TODO: different behavior if match is True
+            else: 
+                # PASS USER
+                tinder.swipe.pass_user(user["user_id"]) 
+                # print("#### Not liked")
+                
+            # Wait between 1 and 10 seconds because we aren't a bot ;)
+            time.sleep(max(1, random.random() * 10))
+
+        # Swipe again tommorow!
+        wait_time = hrs_between_swipes * 60 * 60 + random.randint(0, 3600)
+        time.sleep(wait_time)
